b43f1e85eb2262e57fca5c47e636b9fb4c061f27
Giuli Ilustre <ilustre.giuli@gmail.com>
2023-04-24 17:01:42 -0300

    retirado tags html com PHP embutido

--- app/model/Concurso.php
+++ app/model/Concurso.php
@@ -1,14 +1,11 @@
 <?php
 
-namespace App\Model;
-
-use App\Model\Sorteio;
+require_once __DIR__ . 'app/model/Sorteio.php';
 
 class Concurso
 {
     public function realizaConcurso(): array
-    {
-        require __DIR__ . '/view/index.php';
+    { 
         $sorteio = new Sorteio();
         $sorteio->handle();
         return $sorteio->getnumerosSorteados();
--- app/view/index.php
+++ app/view/index.php
@@ -1,30 +1,20 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MEGA SENA DO GIULI</title>
-</head>
-<body>
-    <table>
+<?php
+        require_once __DIR__ . 'app/model/Concurso.php';
+        require_once __DIR__ . 'app/model/Sorteio.php';
+        $concurso = new Concurso();
+        $sorteio = $concurso->realizaConcurso();
+
+        echo '<table>
         <thead>
             <tr>
                 <th>Dezenas sorteadas</th> 
             </tr>
-        </thead>    
-        <?php
-        require __DIR__ . '/model/Concurso.php';
-        $concurso = new Concurso();
-        $sorteio = $concurso->realizaConcurso();
-
+        </thead>'; 
         foreach ($sorteio  as $dezenaSorteada) {
             echo '<tr>';
             echo    '<td>' . $dezenaSorteada . '</td>';
             echo '</tr>';
         }
-
-        ?>
-    </table>
-</body>
-</html>
+        echo '</table>';
+?>
+       
